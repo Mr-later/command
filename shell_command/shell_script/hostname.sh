6029a9efc8b7985e00502d1dd819a48af0849133
Aurora <xiaerjiao66@163.com>
2019-11-09 23:08:07 +0800

    modify hostname.sh

--- shell_command/shell_script/hostname.sh
+++ shell_command/shell_script/hostname.sh
@@ -1,7 +1,9 @@
 #execute this script in which has the  id_isa  of  other hosts.
 #hosts.txt  is  args .  ($1 is the ip list which is splited by \n or \r\n)
 #execute: sh  hostname.sh  hosts.txt
-
+#-----
+#在拥有私钥的机器上执行此脚本，区分用户，hosts.txt为远程主机列表，一行为一个ip
+#执行方式：sh  hostname.sh  hosts.txt
 #!/bin/bash
 for HOST in `cat hosts.txt`
 do
