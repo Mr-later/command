df9d0510bc6c027a248725aa7de4fd2091222baa
Aurora <xiaerjiao66@163.com>
2019-11-10 01:19:44 +0800

    add telnet_Nto_1_eng.sh

---
+++ shell_command/shell_script/telnet_Nto1_eng.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+for  i in `cat tel.txt`
+do
+
+#echo "$i --> $1 $2 filewalld policy"
+echo "$i --> $1 $2 filewalld policy" >> telnet.log
+#ssh  user@$i  "(sleep 0.5;) | telnet $1 $2  2>&1"
+ssh  user@$i  "(sleep 0.5;) | telnet $1 $2  2>&1" >> telnet.log
+#echo ''
+echo '' >> telnet.log
+done
+
+
+
+#待改进，可操作成NtoN
+
+#在有远程主机的私钥的主机上执行脚本
+
+#执行参数
+#tel.txt:源端的ip
+#$1:目标端ip
+#$2:目标端port
+
+#执行结果:cat telnet.log
+#10.0.41.198 --> 10.0.41.144 5044 filewalld policy
+#Trying 10.0.41.144...
+#Connected to 10.0.41.144.
+#Escape character is '^]'.
+#Connection closed by foreign host.
+
