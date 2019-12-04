2f0338ee8e5fe5254ddc29d89110d18b4b188dd7
xiaerjiao <xiaerjiao66@163.com>
2019-12-05 00:06:48 +0800

    filebeat安装以及启动 和 修改配置文件

--- shell_command/shell_script/init_start_Filebeat.sh
+++ shell_command/shell_script/init_start_Filebeat.sh
@@ -21,9 +21,26 @@ output.logstash:
 EOF
 echo " nohup ${filebeat_path}/filebeat -e -c ${filebeat_path}/filebeat.yml >${filebeat_path}/filebeat.log 2>&1 &" > startFilebeat.sh
 echo "export serverip=`ip addr  |  grep "10.10" | grep  "inet" |  awk   '{print $2}' | awk -F"/" '{print$1}'`"  >> ~/.bash_profile
+
+#source 执行 使serverip生效
 source ~/.bash_profile
 echo ${serverip}
+
+#执行 startFilebeat.sh脚本,若要是将init与start分开则将此行摘出来即可,单独放置一个脚本.
 cd ${filebeat_path}; sh startFilebeat.sh
 
 
+#脚本说明:
 #描述:在A主机远程B1,B2,B3主机，安装filebeat并配置
+
+#第一步：将安装包传至远程服务器
+#scp filebeat-5.6.9-linux-x86_64.tar.gz  hlwztprd@10.0.40.99:/app/hlwztprd
+#第二步：调用init_start_Filebeat.sh脚本,将远程主机的配置配好 并 启动服务.
+ssh  user@ip  < init_start_Filebeat.sh >show.log
+#第三步:在远程主机验证 ps  -ef  |  grep filebeat 时有没有进程的情况,此时重新开启一个终端登录即可.
+
+
+#问题:
+#写入远程主机中的filebeat.yml文件中的变量${serverip}到远程主机中查看时已经成为了ip字符串
+
+
---
+++ shell_command/shell_script/update_Filebeatyml.sh
@@ -0,0 +1,44 @@
+# cat updateFilebeatyml.sh
+
+base_path=/app/hlwztprd
+filebeat_path=$base_path/filebeat
+cd $filebeat_path
+cat <<EOF >filebeat.yml
+filebeat.prospectors:
+- input_type: log
+  paths:
+    - /app/hlwztprd/filebeat/log/*.log
+  ignore_older: 70h
+  close_inactive: 1m
+  close_removed: true
+  clean_inactive: 72h
+  clean_removed: true
+  close_timeout: 3h
+filebeat.spool_size: 256
+fields_under_root: true
+fields:
+  host: $serverip
+loadbalance: true
+output.logstash:
+  hosts: ["ip1:5044","ip2:5044","ip3:5044"]
+EOF
+echo '----------'
+cat filebeat.yml
+filebeatport=$(ps -ef | grep -v grep | grep "filebeat.yml" | sed -n '1P' | awk '{print $2}')
+echo '----------'
+echo "占用端口$filebeatport"
+if [ "" != "$filebeatport" ]
+    then
+        kill -9 $filebeatport
+fi
+nohup $filebeat_path/filebeat -e -c $filebeat_path/filebeat.yml >$filebeat_path/filebeat.log 2>&1 &
+echo "nohup $filebeat_path/filebeat -e -c $filebeat_path/filebeat.yml >$filebeat_path/filebeat.log 2>&1 &"
+echo '####################################################################################################'
+exit 0
+
+
+#脚本功能:在多个远程主机已经启动了filebeat的情况下，改掉配置文件,并重新启动应用
+
+#在jenkins中执行以下命令是会直接有回显结果的
+#有多个ip可以直接写多行
+ssh -t user@ip  < updateFilebeatyml.sh
