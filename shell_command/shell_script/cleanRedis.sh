7a0f59e8b467a737447fc2bfaa803479ec2b4249
Aurora <123@.com>
2019-12-06 16:50:43 +0800

    modify cleanRedis.sh cleanRedis.txt

---
+++ shell_command/shell_script/cleanRedis.sh
@@ -0,0 +1,26 @@
+清理多套环境的redis缓存，同一key。
+
+#清理同样的key，不同的环境(四套)，$1为key
+echo  '################################################################'
+date +"%Y-%m-%d %H:%M:%S"
+address[0]='ip0'
+address[1]='ip1'
+address[2]='ip2'
+address[3]='ip3'
+address[4]='ip4'
+address[5]='ip5'
+address[6]='ip6'
+address[7]='ip7'
+address[8]='ip8'
+address[9]='ip9'
+address[10]='ip10'
+address[11]='ip11'
+
+for i in ${address[@]}
+do
+    echo "${i}"
+    /redis/redis-cli -h ${i} -p 6379 eval "local keys = redis.call('keys', ARGV[1])  for i=1,#keys,5000 do  redis.call('del', unpack(keys, i, math.min(i+4999, #keys)))  end  return keys" 0  *${1}*
+done
+
+
+
